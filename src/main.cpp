2509dfcb8d88f595a3fdc060a48840544d5266ee
UserNameHere <email@example.com>
2024-07-15 12:58:59 -0500

    set supported version of OGL to be 4.5 added clear color to event loop test GLAD gl functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_timer.h>
+#include <glm/trigonometric.hpp>
 #include <stdlib.h>
 #define GLAD_GL_IMPLEMENTATION
 #include "glad/gl.h"
@@ -12,7 +15,7 @@ int main(int argc, char* argv[])
 	SDL_Init(SDL_INIT_EVERYTHING);
 
 	SDL_Window* window  = SDL_CreateWindow("Window",
-	 SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,  600, 400, 0);
+	 SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,  600, 400, SDL_WINDOW_OPENGL);
 
 	SDL_Event event;
 	bool quit = false;
@@ -24,6 +27,13 @@ int main(int argc, char* argv[])
 
 	int version = gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
 
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);	
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION,4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION,5);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,SDL_GL_CONTEXT_PROFILE_CORE);
+
+	 
+
 	while (!quit)
 	{
 		SDL_PollEvent(&event);
@@ -32,6 +42,15 @@ int main(int argc, char* argv[])
 		{
 			quit = true;
 		}
+
+		glClearColor(1,0,1,1);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		SDL_GL_SwapWindow(window);
+		SDL_Delay(1);
 	}
+
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
 	return 0;
 }
