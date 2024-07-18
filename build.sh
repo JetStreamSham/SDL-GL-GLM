5f4ca55cd51ed60a2c7fe4bcf2866b607ff219ec
Jet <76616771+JetStreamSham@users.noreply.github.com>
2024-07-17 19:59:09 -0500

    build.sh now builds imgui fixed build.sh it now paths to proper subdirectory for base project. build.sh renamed some variables

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-build_command="g++ -o ./bin/main.out ./src/*.cpp -lSDL2 -I./include"
+build_baseProject="g++ -o ./bin/main.out ./SDL-GL-GLM/src/*.cpp -lSDL2 -I.//SDL-GL-GLM/include"
+build_imgui="g++ -c ./imgui/*.cpp  ./imgui/backends/imgui_impl_opengl3.cpp ./imgui/backends/imgui_impl_sdl2.cpp -I./imgui `sdl2-config --cflags --libs`"
 
 updateCompileCommands()
 {
-    bear -- $build_command
+    bear -- $build_baseProject
+    bear --append -- $build_imgui
 }
 
 build ()
@@ -17,17 +19,27 @@ build ()
             echo "could not delete bin folder and contents"
         fi
     fi
-    
     mkdir ./bin
+    
+    mkdir ./obj
+    mkdir ./obj/imgui
+
+    echo $build_imgui
+
+    ($build_imgui)
+
+    mv ./*.o ./obj/imgui
+    
 
 #build the thing
-    echo $build_command
-    ($build_command)
+    echo $build_baseProject
+    ($build_baseProject)
 
     if test ! ./bin/main.out 
     then
         echo "Could not build project"
     fi
+
 }
 
 if  [ "$1" = "update" ]
