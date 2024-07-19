2fc51fe03847c0d7c7a054b02bed49ae0b4747fc
Jet <76616771+JetStreamSham@users.noreply.github.com>
2024-07-18 21:24:07 -0500

    build.sh combined build and bear command, since bear runs the build anyway

--- build.sh
+++ build.sh
@@ -1,39 +1,34 @@
 #!/bin/bash
-build_baseProject="g++ -o ./bin/main.out ./SDL-GL-GLM/src/*.cpp -lSDL2 -I.//SDL-GL-GLM/include"
+build_baseProject="g++ -o ./bin/main.out ./SDL-GL-GLM/src/*.cpp -lSDL2 -I.//SDL-GL-GLM/include -I./imgui"
 build_imgui="g++ -c ./imgui/*.cpp  ./imgui/backends/imgui_impl_opengl3.cpp ./imgui/backends/imgui_impl_sdl2.cpp -I./imgui `sdl2-config --cflags --libs`"
-
-updateCompileCommands()
-{
-    bear -- $build_baseProject
-    bear --append -- $build_imgui
-}
 
 build ()
 {
-    if test -d ./bin 
-    then
-        rm -rf --dir ./bin/*
-        rm -rf --dir ./bin
-        if test -d ./bin 
-        then
-            echo "could not delete bin folder and contents"
-        fi
-    fi
-    mkdir ./bin
-    
+
+    mkdir ./bin    
     mkdir ./obj
     mkdir ./obj/imgui
 
     echo $build_imgui
 
-    ($build_imgui)
+    if hash bear
+    then
+        bear --append -- $build_imgui
+    else 
+        ($build_imgui)
+    fi
 
     mv ./*.o ./obj/imgui
     
-
 #build the thing
     echo $build_baseProject
-    ($build_baseProject)
+
+    if hash bear
+    then
+        bear --append -- $build_baseProject
+    else 
+        ($build_baseProject)
+    fi
 
     if test ! ./bin/main.out 
     then
@@ -42,9 +37,4 @@ build ()
 
 }
 
-if  [ "$1" = "update" ]
-then
-    updateCompileCommands
-else
-    build
-fi
+build
